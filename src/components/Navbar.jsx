c83b0877a492f3d9d8c4690f3ae519a06e390514
Anna Turek <anutkakpi@ukr.net>
2024-11-05 19:54:02 +0200

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,6 @@ function Navbar() {
     <nav className="bg-blue-600 text-white shadow-md fixed top-0 left-0 w-full z-50">
       <div className="flex justify-between h-20 items-center px-4">
         <div className="flex items-center space-x-2 w-1/4">
-
           {/* Home Button (Logo) */}
           <button className="flex items-center text-l py-1">
             <img src={logo} alt="Logo" className="h-8 w-auto" />
@@ -13,21 +12,20 @@ function Navbar() {
         </div>
 
         <div className="flex justify-center w-1/2">
-        <span className="text-xl">
-            Cohort Tools
-          </span>
+          <span className="text-xl">Cohort Tools</span>
         </div>
 
         <div className="w-1/4 flex justify-end mr-4">
           {/* User Profile Button */}
-          <button className="flex items-center text-l py-1">
-            <img
-              src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/lab-cohort-tools-routing/profile-1.png"
-              alt="Logo"
-              className="h-10 w-auto border-solid border border-white rounded-3xl p-1"
-            />
-          </button>
-
+          <Link to="/profile">
+            <button className="flex items-center text-l py-1">
+              <img
+                src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/lab-cohort-tools-routing/profile-1.png"
+                alt="Logo"
+                className="h-10 w-auto border-solid border border-white rounded-3xl p-1"
+              />
+            </button>
+          </Link>
         </div>
       </div>
     </nav>
